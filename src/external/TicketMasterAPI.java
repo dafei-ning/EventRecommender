20bbc82d40fcaaedac7b7055dc88097699c7b51a
Dafei Ning <ningdafei@gmail.com>
2018-08-12 13:46:25 -0700

    update API

--- src/external/TicketMasterAPI.java
+++ src/external/TicketMasterAPI.java
@@ -4,9 +4,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import entity.Item;
+import entity.Item.ItemBuilder;
 
 public class TicketMasterAPI {
 	private static final String URL = "https://app.ticketmaster.com/discovery/v2/events.json";
@@ -14,9 +22,27 @@ public class TicketMasterAPI {
 	// from TicketMaser
 	private static final String API_KEY = "KAIALFNsDitUZR3QAnuitMQEehmmBauB";
 	
+	private static final String EMBEDDED = "_embedded";
+	private static final String EVENTS = "events";
+	private static final String NAME = "name";
+	private static final String ID = "id";
+	private static final String URL_STR = "url";
+	private static final String RATING = "rating";
+	private static final String DISTANCE = "distance";
+	private static final String VENUES = "venues";
+	private static final String ADDRESS = "address";
+	private static final String LINE1 = "line1";
+	private static final String LINE2 = "line2";
+	private static final String LINE3 = "line3";
+	private static final String CITY = "city";
+	private static final String IMAGES = "images";
+	private static final String CLASSIFICATIONS = "classifications";
+	private static final String SEGMENT = "segment";
+
+	
 	
 	//首先确定response返回的是JSON，这里的keyword就是要查询的输入string
-	public JSONArray search(double lat, double lon, String keyword) {
+	public List<Item> search(double lat, double lon, String keyword) {
 		//Encode keyword in URL 因为有可能有特殊字符
 		if (keyword == null) {
 			keyword = DEFAULT_KEYWORD;
@@ -61,30 +87,183 @@ public class TicketMasterAPI {
 			JSONObject obj = new JSONObject(response.toString());
 			//如果不存在里面有 embedded的，就返回一个空的JSONArray
 			if (obj.isNull("_embedded")) {
-				return new JSONArray();
+				return new ArrayList<>();
 			}
 			
 			// 如果存在，就转换成event
 			JSONObject embedded = obj.getJSONObject("_embedded");
 			JSONArray events = embedded.getJSONArray("events");
 			
-			return events;
+			return getItemList(events);
 			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		return new JSONArray();
+		return new ArrayList<>();
+	}
+	
+	
+	/**
+	 * Helper methods
+	 */
+
+	//  {
+	//    "name": "laioffer",
+              //    "id": "12345",
+              //    "url": "www.laioffer.com",
+	//    ...
+	//    "_embedded": {
+	//	    "venues": [
+	//	        {
+	//		        "address": {
+	//		           "line1": "101 First St,",
+	//		           "line2": "Suite 101",
+	//		           "line3": "...",
+	//		        },
+	//		        "city": {
+	//		        	"name": "San Francisco"
+	//		        }
+	//		        ...
+	//	        },
+	//	        ...
+	//	    ]
+	//    }
+	//    ...
+	//  }
+	// TicketMaster里本身API就很凌乱
+	// Address应该是隐藏的结构最深的
+	private String getAddress(JSONObject event) throws JSONException {
+		if (!event.isNull(EMBEDDED)) {
+			JSONObject embedded = event.getJSONObject(EMBEDDED);
+			
+			if(!embedded.isNull(VENUES)) {
+				JSONArray venues = embedded.getJSONArray(VENUES);
+				
+				for (int i = 0; i< venues.length(); i++) {
+					JSONObject venue = venues.getJSONObject(i);
+					StringBuilder sBuilder = new StringBuilder();
+					
+					if(!venue.isNull(ADDRESS)) {
+						JSONObject address = venue.getJSONObject(ADDRESS);
+						if(!address.isNull(LINE1)) {
+							sBuilder.append(address.getString(LINE1));
+						}
+						if(!address.isNull(LINE2)) {
+							sBuilder.append(address.getString(LINE2));
+						}
+						if(!address.isNull(LINE3)) {
+							sBuilder.append(address.getString(LINE3));
+						}
+								
+					}
+					
+					if(!venue.isNull(CITY)) {
+						JSONObject city = venue.getJSONObject(CITY);
+						if(!city.isNull(NAME)) {
+							sBuilder.append("\n");
+							sBuilder.append(city.getString(NAME));
+						}
+					}
+					String addr = sBuilder.toString();
+					if(addr.length() > 0) {
+						return addr;
+					}
+				}
+				
+			}
+		}
+		return ""; // 如果没有地址，返回空string
+	}
+	
+	// {"images": [{"url": "www.example.com/my_image.jpg"}, ...]}
+	private String getImageUrl(JSONObject event) throws JSONException {
+		if (!event.isNull(IMAGES)) {
+			JSONArray images = event.getJSONArray(IMAGES);
+			
+			for (int i = 0; i < images.length(); i++) {
+				JSONObject image = images.getJSONObject(i);
+				StringBuilder sBuilder = new StringBuilder();
+				if (!image.isNull(URL_STR)) {
+					sBuilder.append(image.getString(URL_STR));
+				}
+				String sbb = sBuilder.toString();
+				if(sbb.length() > 0) {
+					return sbb;
+				}
+			}
+		}		
+		return "";
+	}
+	
+	// {"classifications" : [{"segment": {"name": "music"}}, ...]}
+	// classification 里面只找segment，segment里面只找name
+	private Set<String> getCategories(JSONObject event) throws JSONException {
+		Set<String> categories = new HashSet<>();
+
+		if (!event.isNull(CLASSIFICATIONS)) {
+			JSONArray classifications = event.getJSONArray(CLASSIFICATIONS);
+			
+			for (int i = 0; i < classifications.length(); ++i) {
+				JSONObject classification = classifications.getJSONObject(i);
+				
+				if (!classification.isNull(SEGMENT)) {
+					JSONObject segment = classification.getJSONObject(SEGMENT);
+					
+					if (!segment.isNull(NAME)) {
+						categories.add(segment.getString(NAME));
+					}
+				}
+			}
+		}
+		return categories;
+	}
+	
+	// Convert JSONArray to a list of item objects.
+	private List<Item> getItemList(JSONArray events) throws JSONException {
+		List<Item> itemList = new ArrayList<>();
+		
+		for (int i = 0; i < events.length(); ++i) {
+			JSONObject event = events.getJSONObject(i);
+			
+			ItemBuilder builder = new ItemBuilder();
+			
+			if (!event.isNull(NAME)) {
+				builder.setName(event.getString(NAME));
+			}
+			if (!event.isNull(ID)) {
+				builder.setItemId(event.getString(ID));
+			}
+			if (!event.isNull(URL_STR)) {
+				builder.setUrl(event.getString(URL_STR));
+			}
+			if (!event.isNull(RATING)) {
+				builder.setRating(event.getDouble(RATING));
+			}
+			if (!event.isNull(DISTANCE)) {
+				builder.setDistance(event.getDouble(DISTANCE));
+			}
+			
+			builder.setAddress(getAddress(event));
+			builder.setCategories(getCategories(event));
+			builder.setImageUrl(getImageUrl(event));
+			
+			//把爬下来的东西赋值到新建的builder（新建了一个item）里
+			itemList.add(builder.build());
+		}
+		return itemList;
 	}
 	
 	// 下面这个函数主要用来检测search JSON是否会成功
 	private void queryAPI(double lat, double lon) {
-		JSONArray events = search(lat, lon, null); // 为什么后面要加null
+		//JSONArray events = search(lat, lon, null); 
+		List<Item> itemList = search(lat, lon, null); // 为什么后面要加null
 		try {
-			for (int i = 0; i < events.length(); ++i) {
-				JSONObject event = events.getJSONObject(i);
-				System.out.println(event);
+			for (Item item: itemList) {
+				//JSONObject event = events.getJSONObject(i);
+				JSONObject jsonObject = item.toJSONObject();
+				System.out.println(jsonObject);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -101,7 +280,7 @@ public class TicketMasterAPI {
 		// London, UK
 		// tmApi.queryAPI(51.503364, -0.12);
 		// Houston, TX
-		tmApi.queryAPI(37.38, -122.08);
+		tmApi.queryAPI(51.503364, -0.12);
 	}
 
 
